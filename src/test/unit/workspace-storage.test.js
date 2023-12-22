753e5c24a6921a0ca0e93391a7ba3e87cee9cbaf
Elec0 <elec08215@gmail.com>
2023-12-21 18:34:45 -0800

    Add unit tests for WorkspaceStorage class

---
+++ src/test/unit/workspace-storage.test.js
@@ -0,0 +1,64 @@
+import { Workspace } from "../../obj/workspace";
+import { WorkspaceStorage } from "../../workspace-storage";
+
+describe('WorkspaceStorage', () => {
+    let workspaceStorage;
+    let workspace;
+
+    beforeEach(() => {
+        workspaceStorage = new WorkspaceStorage();
+        workspace = new Workspace(); // Initialize Workspace with appropriate values
+    });
+    afterEach(() => {
+        workspaceStorage.clear();
+    });
+
+    test('should initialize with size 0', () => {
+        expect(workspaceStorage.size).toBe(0);
+    });
+
+    test('should add a workspace and increase size', () => {
+        workspaceStorage.set('test', workspace);
+        expect(workspaceStorage.size).toBe(1);
+    });
+
+    test('should retrieve a workspace by key', () => {
+        workspaceStorage.set('test', workspace);
+        expect(workspaceStorage.get('test')).toBe(workspace);
+    });
+
+    test('should delete a workspace by key', () => {
+        workspaceStorage.set('test', workspace);
+        workspaceStorage.delete('test');
+        expect(workspaceStorage.get('test')).toBeUndefined();
+    });
+
+    test('should clear all workspaces', () => {
+        workspaceStorage.set('test', workspace);
+        workspaceStorage.clear();
+        expect(workspaceStorage.size).toBe(0);
+    });
+
+    test('should check if a workspace exists', () => {
+        workspaceStorage.set('test', workspace);
+        expect(workspaceStorage.has('test')).toBe(true);
+    });
+
+    test('should iterate over workspaces', () => {
+        workspaceStorage.set('test', workspace);
+        const entries = [...workspaceStorage];
+        expect(entries).toEqual([['test', workspace], [undefined, workspace]]);
+    });
+
+    test('should return all keys', () => {
+        workspaceStorage.set('test', workspace);
+        const keys = [...workspaceStorage.keys()];
+        expect(keys).toEqual(['test']);
+    });
+
+    test('should return all values', () => {
+        workspaceStorage.set('test', workspace);
+        const values = [...workspaceStorage.values()];
+        expect(values).toEqual([workspace]);
+    });
+});
