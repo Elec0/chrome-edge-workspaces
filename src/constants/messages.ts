176464303583583e9e2409be8cf845889087323a
Elec0 <elec08215@gmail.com>
2023-12-29 16:28:45 -0800

    Add new message constant for opening workspace

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -1,4 +1,5 @@
 export class Messages {
     public static MSG_NEW_WORKSPACE = "NEW_WORKSPACE";
     public static MSG_GET_WORKSPACES = "GET_WORKSPACES";
+    public static MSG_OPEN_WORKSPACE = "OPEN_WORKSPACE";
 }
