24b52e7dccf2e01700f84998b4835e6ad4384492
Elec0 <elec08215@gmail.com>
2023-12-31 16:38:23 -0800

    Add ESLint and update configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,27 @@
-module.exports =  {
-    plugins: [
-      "@typescript-eslint/eslint-plugin",
-      "eslint-plugin-tsdoc"
-    ],
-    extends:  [
-      'plugin:@typescript-eslint/recommended'
-    ],
-    parser:  '@typescript-eslint/parser',
-    parserOptions: {
-      project: "./tsconfig.json",
-      tsconfigRootDir: __dirname,
-      ecmaVersion: 2018,
-      sourceType: "module"
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
     },
-    rules: {
-      "tsdoc/syntax": "warn"
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "overrides": [
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint",
+        "eslint-plugin-tsdoc"
+    ],
+    "rules": {
+        "tsdoc/syntax": "warn",
+        // Note: you must disable the base rule as it can report incorrect errors
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": "off",
     }
-  };
+}
