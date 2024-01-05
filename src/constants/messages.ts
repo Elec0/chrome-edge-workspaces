a31b9b0eb59c3f96a30cea0b3e00382b7d69ff7e
Elec0 <elec08215@gmail.com>
2024-01-05 12:37:12 -0800

    Add message for getting workspaces

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -1,5 +1,12 @@
 export class Messages {
     public static MSG_NEW_WORKSPACE = "NEW_WORKSPACE";
+    /**
+     * 
+     * Send a message to the background script requesting all workspace data.
+     * @returns A Promise that resolves to a MessageResponse object, containing a 
+     * @see BackgroundMessageHandlers.processGetWorkspaces
+     * @see PopupMessageHelper.sendGetWorkspaces
+     */
     public static MSG_GET_WORKSPACES = "GET_WORKSPACES";
     public static MSG_OPEN_WORKSPACE = "OPEN_WORKSPACE";
 }
