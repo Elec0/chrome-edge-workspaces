a850676dc51ddf903d313f9d94eb0016c41c48f9
Elec0 <elec08215@gmail.com>
2024-02-12 11:26:30 -0800

    Finally have a working e2e test with new window and everything

--- src/test/e2e/basic-workflow.test.ts
+++ src/test/e2e/basic-workflow.test.ts
@@ -1,20 +1,41 @@
 import { assert } from "console";
-import puppeteer, { Browser } from 'puppeteer';
+import puppeteer, { Browser, Page } from 'puppeteer';
 
 const EXTENSION_PATH = `${process.cwd()}/build`;
 const EXTENSION_ID = 'laobpiaijpjcdllfnphlmjoaofilopmi';
 
-let browser: Browser | undefined;
+let browser: Browser;
+let page: Page;
 
 beforeEach(async () => {
     // https://pptr.dev/guides/debugging
     browser = await puppeteer.launch({
         // slowMo: 250, // slow down by 250ms
-        headless: "new",
+        headless: false,
+        // headless: "new",
         args: [
             `--disable-extensions-except=${EXTENSION_PATH}`,
             `--load-extension=${EXTENSION_PATH}`
         ]
+    });
+
+    if (!browser) {
+        assert(browser);
+        return;
+    }
+
+    page = await browser.newPage();
+    await page.goto(`chrome-extension://${EXTENSION_ID}/popup.html`);
+    
+    if (!page) {
+        assert(page);
+        return;
+    }
+
+    // Clear the local and sync storage before each test
+    await page.evaluate(() => {
+        chrome.storage.local.clear();
+        chrome.storage.sync.clear();
     });
 });
 
@@ -23,34 +44,51 @@ beforeEach(async () => {
 
 afterEach(async () => {
     await browser?.close();
-    browser = undefined;
 });
 
 test("creating a new workspace adds it to the list", async () => {
-    if (!browser) {
-        assert(browser);
-        return;
-    }
-    const page = await browser.newPage();
-    await page.goto(`chrome-extension://${EXTENSION_ID}/popup.html`);
-
-    // Enter 'test workspace' in the input box, when it appears
-    page.on("dialog", async (dialog) => {
-        await dialog.accept("test workspace");
-    });
-
     // Click the button to add a workspace
-    const btn = await page.$("#addBtn");
+    const btn = await page.$("#addWorkspace");
     expect(await btn?.isVisible()).toBe(true);
     await btn?.click();
 
+    // Setup listener for the new window (browser) popup
+    const newWindowPagePromise = new Promise<Page>((resolve) => {
+        browser?.on("targetcreated", async (target) => {
+            const newPage = await target.page();
+            if (newPage) {
+                resolve(newPage);
+            }
+        });
+    });
+
+    // Verify the dialog is visible
+    const dialog = await page.$("dialog");
+    expect(await dialog?.isVisible()).toBe(true);
+
+    // Enter the name of the new workspace
+    await page.type("#modal-input-name", "test workspace");
+
+    // Click the submit input
+    await page.click("#modal-submit");
+
+    // Wait for the new window to open
+    const newPage = await newWindowPagePromise;
+    expect(newPage).toBeDefined();
+
+    // Navigate to google.com
+    await newPage?.goto("https://www.google.com", { waitUntil: 'domcontentloaded' });
+    
+    // Close the new window
+    await newPage?.close();
+
     // Verify there is a new workspace in the list, and that it has the correct name
-    const list = await page.$("#workspaces");
+    const list = await page.$("#workspaces-list");
     expect(await list?.isVisible()).toBe(true);
-    // Find the text of the workspace name
+
+    // Use ElementHandler.waitForSelector to wait for the new workspace text to appear
     const workspaceBtn = await list?.waitForSelector("xpath///div[contains(text(), 'test workspace')]");
-    // Verify there are "1 tabs" in the workspace button text, since it's a new workspace
     expect(await workspaceBtn?.evaluate((el) => el.textContent)).toContain("1 tabs");
 
-    await browser.close();
+    await browser?.close();
 });
