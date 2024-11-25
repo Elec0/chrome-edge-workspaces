aa19b0e4be2b8444323ff7fb10caf6e3f7005bfd
Elec0 <elec08215@gmail.com>
2024-11-25 14:53:50 -0800

    Update E2E test extension ID with the static one from chrome webstore

--- src/test/e2e/utils/e2e-constants.ts
+++ src/test/e2e/utils/e2e-constants.ts
@@ -1,5 +1,5 @@
 
 export class E2EConstants {
     public static EXTENSION_PATH = `${ process.cwd() }/build/chrome`;
-    public static EXTENSION_ID = 'ognnkoophhofbamdhhgbcaglbmpjcpak';
+    public static EXTENSION_ID = 'feehlkcbifmladjmmpkghfokcngfkkkp';
 }
